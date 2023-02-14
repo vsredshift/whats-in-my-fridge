967b63811c9f6600c0d5ed8bade0e047eaeba234
Martin O`Shaughnessy <vsredshift@gmail.com>
2023-02-14 17:01:07 +0100

    add async handler

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,36 +1,38 @@
+const asyncHandler = require("express-async-handler")
 
 // @desc    Get products
 // @route   GET /v1/api/products
 // @access  Private
-const getProducts = (req, res) => {
+const getProducts = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Get products" })
-}
+})
 
 // @desc    Set product
 // @route   POST /v1/api/products
 // @access  Private
-const setProduct = async (req, res) => {
+const setProduct = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error("Please add text field")
     }
 
     res.status(201).json({ message: "Set product" })
-}
+})
 
 // @desc    Update product
 // @route   PUT /v1/api/products/:id
 // @access  Private
-const updateProduct = (req, res) => {
+const updateProduct = asyncHandler(async (req, res) => {
     res.status(201).json({ message: `Update product ${req.params.id}` })
-}
+})
 
 // @desc    Delete product
 // @route   DELETE /v1/api/products/:id
 // @access  Private
-const deleteProduct = (req, res) => {
+const deleteProduct = asyncHandler(async (req, res) => {
     res.status(201).json({ message: `Delete product ${req.params.id}` })
-}
+})
+
 module.exports = {
     getProducts,
     setProduct,
