7e4e7fa3d703a8755b39b9e408587dc1cc9684bb
Martin O`Shaughnessy <vsredshift@gmail.com>
2023-02-18 19:29:27 +0100

    Update register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,34 +1,112 @@
-import { useState, useEffect } from "react"
-import { FaUser } from "react-icons/fa"
+import { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
 
 const Register = () => {
-    const [formData, setFormDate] = useState({
-        username: '',
-        email: '',
-        password: '',
-        password2: ''
-    })
+  const [formData, setFormData] = useState({
+    username: "",
+    fullname: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
 
-    const { username, email, password, password2 } = formData
+  const { username, fullname, email, password, password2 } = formData;
 
-    const onChange = () => {
-        
-    }
-    return <>
-        <section className="heading">
-            <h1>
-                <FaUser />Register
-            </h1>
-            <p>Create an account here</p>
-        </section>
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-        <section className="form">
-            <form>
-                <input type="text" className="form-control" id="username" name="username" value={username} placeholder="Enter username" onChange={onC
-                }/>
-            </form>
-        </section>
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser />
+          Register
+        </h1>
+        <p>Create an account here</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              name="username"
+              value={username}
+              placeholder="Enter username"
+              required
+              aria-label="username"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="fullname"
+              name="fullname"
+              value={fullname}
+              placeholder="Enter full name"
+              aria-label="fullname"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter email"
+              required
+              aria-label="email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter a password"
+              required
+              aria-label="password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password2"
+              className="form-control"
+              id="password2"
+              name="password2"
+              value={password2}
+              placeholder="Confirm password"
+              required
+              aria-label="confirm password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
     </>
-}
+  );
+};
 
-export default Register
+export default Register;
